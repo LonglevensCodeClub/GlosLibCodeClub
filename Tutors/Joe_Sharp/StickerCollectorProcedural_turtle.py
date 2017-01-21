2248fd2f36de1ffc107b8595f163612d300478f6
Joe Sharp <ratracejoe@hotmail.com>
2017-01-21 22:54:47 +0000

    refining turtle

--- Tutors/Joe_Sharp/StickerCollectorProcedural_turtle.py
+++ Tutors/Joe_Sharp/StickerCollectorProcedural_turtle.py
@@ -8,12 +8,12 @@ stickerTurtles = []
 
 def observer(whichSticker, count):
 	print("Sticker: {}, Count: {}".format(whichSticker, count))
-	thisColour = "black" if count == 0 else "green"
+	thisColour = "red" if count == 0 else "green"
 	stickerTurtle = stickerTurtles[whichSticker-1]
 	stickerTurtle.color(thisColour)
-	stickerTurtle.turtlesize(stretch_wid=1 + count)
+	stickerTurtle.turtlesize(stretch_wid=1 + (count / 3))
 	
-myBookSize = 64
+myBookSize = 16
 wallDim = math.floor(math.sqrt(myBookSize))
 spaceToFill=600
 halfSpaceToFill = spaceToFill / 2
@@ -23,6 +23,7 @@ for i in range(1, myBookSize+1):
 	c = turtle.Turtle()
 	c.shape("turtle")
 	c.penup()
+	c.speed(100)
 	tx = math.floor((i-1) % wallDim)
 	ty = math.floor((i-1) / wallDim)
 	c.setposition(tx * spacing - halfSpaceToFill, ty * spacing - halfSpaceToFill)
@@ -32,5 +33,11 @@ for i in range(1, myBookSize+1):
 stickers, stickersBought = collectStickers(bookSize=myBookSize, observer=observer)
 print("Bought {} stickers for a collection of {}".format(stickersBought, myBookSize))
 print("Collection Looks Like This: {}".format(stickers))
+
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
+writer.setposition(-halfSpaceToFill - 70, halfSpaceToFill + 50)
+writer.write("Bought {} stickers for a collection of {}".format(stickersBought, myBookSize), font=("Arial", 32, "normal"))
 
 turtle.done()
