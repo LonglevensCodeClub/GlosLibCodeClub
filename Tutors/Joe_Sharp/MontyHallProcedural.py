f48d3f86337a0b2bab59dd4d8c2a5118771da9df
Joe Sharp <ratracejoe@hotmail.com>
2017-01-23 21:24:38 +0000

    making the procedural one interactive

--- Tutors/Joe_Sharp/MontyHallProcedural.py
+++ Tutors/Joe_Sharp/MontyHallProcedural.py
@@ -1,6 +1,6 @@
 from random import randint, choice
 
-def playMontyHall(switch=False):
+def playMontyHall(initialGuess=0, switch=False, interactive=False):
 	# Randomly pick a correct door
 	correctDoor = randint(0, 2)
 	
@@ -10,9 +10,6 @@ def playMontyHall(switch=False):
 		if not x == correctDoor:
 			incorrectDoors.append(x)
 			
-	# Randomly pick a choice for the player
-	initialGuess = randint(0, 2)
-	
 	# Which doors have not been chosen and are not the correct door?
 	incorrectDoorsNotChosen = []
 	for x in incorrectDoors:
@@ -22,33 +19,59 @@ def playMontyHall(switch=False):
 	# Host reveals a door (choose from incorrect doors that have not already been chosen)
 	hostReveal = choice(incorrectDoorsNotChosen)
 	
+	# Interactive version
+	if interactive:
+		print("Host has revealed there is nothing behind door {}".format(hostReveal))
+	
 	# Find the available door to 'switch' to
 	for x in range(3):
 		if (not x == hostReveal) and (not x == initialGuess):
 			availableSwitchDoor = x
+			
+	# Interactive version
+	if interactive:
+		switchStr = input("Do you want to switch doors to {} instead of {}? (y/n): ".format(availableSwitchDoor, initialGuess))
+		switch = switchStr in set(["y", "Y", "Yes", "yes"])
 			
 	if switch:
 		finalGuess = availableSwitchDoor
 	else:
 		finalGuess = initialGuess
 		
-	return finalGuess == correctDoor
-
-# Run statistical analysis
-NUMBER_TESTS = 10000
-winsByStrategy = dict()
-
-# For each switching strategy (always True or always False)
-for strategy in set([True, False]):
+	won = (finalGuess == correctDoor)
 	
-	# Run the game a number of times and count the wins
-	winsByStrategy[strategy] = 0
-	for x in range(NUMBER_TESTS):
-		if playMontyHall(strategy):
-			winsByStrategy[strategy] += 1
-			
-print("Wins By Switching: {}".format(winsByStrategy))
+	# Interactive version
+	if interactive:
+		if won:
+			print("Congratulations! You won with door {}".format(finalGuess))
+		else:
+			print("Sorry! You list with door {}, prize was behind {}".format(finalGuess, correctDoor))
 
-for strategy, wins in winsByStrategy.items():
-	winPercent = 100 * wins / NUMBER_TESTS;
-	print("If we always play switch: {} then the chances of winning are {}%".format(strategy, winPercent))
+	return won
+
+initialGuess = int(input("Choose a door 0, 1 or 2: "));
+playMontyHall(initialGuess=initialGuess, interactive=True)
+
+def runStatisticalAnalysis():
+	# Run statistical analysis
+	NUMBER_TESTS = 10000
+	winsByStrategy = dict()
+
+	# For each switching strategy (always True or always False)
+	for strategy in set([True, False]):
+		
+		# Run the game a number of times and count the wins
+		winsByStrategy[strategy] = 0
+		for x in range(NUMBER_TESTS):
+			# Randomly pick a choice for the player
+			initialGuess = randint(0, 2)
+			if playMontyHall(initialGuess=initialGuess, switch=strategy):
+				winsByStrategy[strategy] += 1
+				
+	print("Wins By Switching: {}".format(winsByStrategy))
+
+	for strategy, wins in winsByStrategy.items():
+		winPercent = 100 * wins / NUMBER_TESTS;
+		print("If we always play switch: {} then the chances of winning are {}%".format(strategy, winPercent))
+		
+runStatisticalAnalysis()
