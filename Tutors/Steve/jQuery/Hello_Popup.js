ecc1638efd38376d2ec085851b0c443f74502801
pi.B+ <sjmart1uk@yahoo.co.uk>
2017-02-11 10:51:50 +0000

    jQuery Popup

---
+++ Tutors/Steve/jQuery/Hello_Popup.js
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>My First Popup</title>
+		
+		<!-- Latest compiled and minified CSS -->
+		<link	rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+				crossorigin="anonymous">
+		
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-3.1.1.js"
+				integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="
+				crossorigin="anonymous"></script>
+		
+		<!-- This time our scripot is in a separate file. -->
+		<script type="text/javascript" src="popup.js">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Click this to see our pop-up</h1>
+		</div>
+	</body>
+</html>
