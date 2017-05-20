286af2f37f26214e45019149f97cc295cfb71b17
Raspberry Pi 3 <longlevens.library.code.club@gmail.com>
2017-05-20 11:36:41 +0000

    Harry 20-May

---
+++ Kids/Harry/fidgetspinner.py
@@ -0,0 +1,24 @@
+   <html>
+		<head>
+			<script type="text/javascript">
+			
+   window.onload = function() {
+	const svgMain = document.getElementById("svgMain);
+	const svgMainRect = svgMain.getBoundingClientRect();
+	
+	const SVG_NS = "http://www.w3.org/2000/svg";
+	
+	console.log("SVG Found " = svgMainRect);
+	
+	
+	
+	
+};
+		</script>
+	</head>
+	<body>
+		<svg id="svgMain" width="100% height="100%>
+		
+		</svg>
+	</body>
+</html>
