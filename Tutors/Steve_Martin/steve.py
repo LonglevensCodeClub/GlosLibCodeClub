451aff90f01f7c1fc7055c8f80818fce07e7d7bc
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2017-08-20 21:10:53 +0000

    Test of file modification from new Pi.

--- Tutors/Steve_Martin/steve.py
+++ Tutors/Steve_Martin/steve.py
@@ -5,22 +5,22 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 def doDirectory(dir):
-	contents = glob.glob(dir + "/*")
+    contents = glob.glob(dir + "/*")
 
-	for item in contents:
-		if (os.path.isdir(item)):
-			print("Dir: " + item)
-			doDirectory(item)
-		elif (os.path.isfile(item)):
-			print("File: " + item)
-			if (item.endswith(".gif")):
-				i = Image.open(item)
-				info = i._getexif()
-				for tag, value in info.items():
-					print(TAGS.get(tag, tag))
-		else:
-			print("### Skipping: " + item)
+    for item in contents:
+        if (os.path.isdir(item)):
+            print("Dir: " + item)
+            doDirectory(item)
+        elif (os.path.isfile(item)):
+            print("File: " + item)
+            if (item.endswith(".gif")):
+                i = Image.open(item)
+                info = i._getexif()
+                for tag, value in info.items():
+                    print(TAGS.get(tag, tag))
+        else:
+            print("### Skipping: " + item)
 
 
 doDirectory("/home/pi")
-		
+ 
