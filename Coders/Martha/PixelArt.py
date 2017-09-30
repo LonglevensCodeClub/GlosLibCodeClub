fa9c6c4cd7186d401181370fabb9efc7222a870a
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2017-09-30 11:37:49 +0000

    Martha 30 Step

---
+++ Coders/Martha/PixelArt.py
@@ -0,0 +1,30 @@
+from sense_emu import SenseHat
+import time
+from PIL import Image
+import os
+
+# Open image file
+image_file = os.path.join(
+os.sep, "/home", "pi", "Downloads", "PodgyUnicorn.png")
+img = Image.open(image_file)
+
+# generate rbg values for image pixels
+rbg_img = img.convert ('RGB')
+image_pixels = list(rbg_img.getdata())
+
+# Get the 64 Pixels you need
+pixel_width = 6
+image_width = pixel_width*8
+sense_pixels = []
+start_pixel = 0
+while start_pixel < (image_width*64):
+    sense_pixels.extend(image_pixels[start_pixel:(start_pixel+image_width):pixel_width])
+    start_pixel += (image_width*pixel_width)
+
+# Display the image
+sense = SenseHat()
+sense.set_rotation(r=180)
+sense.set_pixels(sense_pixels)
+time.sleep (3)
+
+sense.clear()
