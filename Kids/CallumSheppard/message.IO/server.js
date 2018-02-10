74d9b0ac8490bbee9260ce1d0a8d557e452c753e
Code Club <longlevens.library.code.club@gmail.com>
2018-02-10 12:38:59 +0000

    socket.io finiched

--- Kids/CallumSheppard/message.IO/server.js
+++ Kids/CallumSheppard/message.IO/server.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+// Route handler
+app.get('/', function (req, res) {
+  // res.send('<h1>Hello Callum</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
+
+
