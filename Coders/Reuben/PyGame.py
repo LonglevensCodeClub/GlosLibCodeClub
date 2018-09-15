dd95aca51116bdbad61d2f962d5743e942aaed43
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2018-09-15 11:37:04 +0000

    15/9/18

---
+++ Coders/Reuben/PyGame.py
@@ -0,0 +1,11 @@
+import random , pygame sys
+from pygame.locals import *
+
+FPS =  30 
+WINDOWWIDTH = 640
+WINFOWWIDTH = 480
+REVEALSPEED = 8
+BOXSIZE = 40
+GAPSIZE =10 
+BOARSWIDTH = 10
+BOARSWIDHT = 7 
