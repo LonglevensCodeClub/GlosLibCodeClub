a06822bb34b65b14964ff1fca06a2e0ed7955e9c
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2018-09-22 12:33:19 +0100

    22/9/18

--- Coders/Reuben/Puzzle .py
+++ Coders/Reuben/Puzzle .py
@@ -1,4 +1,3 @@
-
 import random, pygame, sys
 from pygame.locals import *
 
@@ -11,3 +10,10 @@ GAPSIZE =10
 BOARDWIDTH = 10
 BOARDHEIGHT = 7 
 assert(BOARDWIDTH*BOARDHEIGHT)% 2 == 0, ' Board needs to have an even number of boxesfor pairs of matches.'
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH* (BOXSIZE * (BOXSIZE + GAPSIZE ))) / 2 )
+YMARGIN = int((WINDOWHEIGHT  - (BOARDHEIGTH * (BOXSIZE + GAPSIZE))
+
+ #            R      G      B
+
+GRAY =
+               
