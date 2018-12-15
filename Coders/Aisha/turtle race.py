07b5dd5ce98d308695c3fe7810efe346c987b0b3
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2018-12-15 12:39:06 +0000

    Aisha 15 Dec

--- Coders/Aisha/turtle race.py
+++ Coders/Aisha/turtle race.py
@@ -1,6 +1,19 @@
 
 from turtle import *
 from random import randint
+
+def createTurtle(colour, track, name):
+    turtle=Turtle()
+    turtle.color(colour)
+    turtle.shape('turtle')
+
+    turtle.penup()
+    turtle.goto(-160, 100-30*track)
+    turtle.write(name)
+    turtle.pendown()
+    return turtle
+    
+
 speed(10)
 penup()
 goto(-140, 140)
@@ -9,86 +22,35 @@ for step in range(15):
     right(90)
     forward(10)
     pendown()
-    forward (150)
+    forward (250)
     penup()
-    backward(160)
+    backward(260)
     left(90)
     forward(20)
 
-ada=Turtle()
-ada.color('red')
-ada.shape('turtle')
+names=['ada','ann' ,'kay' ,'aisha' ,'bob','kev' , 'elliot' , 'may']
+colours=['red','turquoise', 'pink', 'purple', 'blue', 'green', 'orange', 'black']
+turtles=[]
+for i in range (len(names)):
+    turtle=createTurtle(colours[i], i, names[i])
+    turtles.append(turtle)
 
-ada.penup()
-ada.goto(-160, 100)
-ada.pendown()
-
-ann=Turtle()
-ann.color('turquoise')
-ann.shape('turtle')
-
-ann.penup()
-ann.goto(-160, 10)
-ann.pendown()
-
-kay=Turtle()
-kay.color('pink')
-kay.shape('turtle')
-
-kay.penup()
-kay.goto(-160, -20)
-kay.pendown()
-
-aisha=Turtle()
-aisha.color('purple')
-aisha.shape('turtle')
-
-aisha.penup()
-aisha.goto(-160, 40)
-aisha.pendown()
-
-bob=Turtle()
-bob.color('blue')
-
-bob.shape('turtle')
-
-bob.penup()
-bob.goto(-160, 70)
-bob.pendown()
-
-
-
-
-
-
+#turtles=[ada,ann ,kay ,aisha ,bob,kev , elliot , may]
+w=0
+winner = 0
+leader = createTurtle('red', 9, 'leader')
 
 for turn in range(100):
-    ada.forward(randint(1,5))
-    bob.forward(randint(1,5))
-    aisha.forward(randint(1,5))
-    ann.forward(randint(1,5))
-    kay.forward(randint(1,5))
-w=0
-winner = "bonus"
-[x,y] = ada.position()
-if x>w:
-    w = x
-    winner="ada"
-[x,y] = bob.position()
-if x>w:
-    w = x
-    winner="bob"
-[x,y] = aisha.position()
-if x>w:
-    w = x
-    winner="aisha"
-[x,y] = kay.position()
-if x>w:
-    w = x
-    winner="kay"
-[x,y] = ann.position()
-if x>w:
-    w = x
-    winner="ann"
+    i = 0
+    for turtle in turtles :
+        i = i + 1
+        turtle.forward(randint(1,5))
+        [x,y] = turtle.position()
+        if x>w:
+            w = x
+            winner=i
+    leader.write(names[winner])
+    
 
-print("the winner is",winner)
+
+print("the winner is", names[winner])
