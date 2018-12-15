41efe2e3a3a6af2920ac3ca631fd5222ad2024d1
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2018-12-15 19:00:10 +0000

    Changes for Aisha 15 Dec 2018

--- Coders/Aisha/turtle race.py
+++ Coders/Aisha/turtle race.py
@@ -1,19 +1,11 @@
-
 from turtle import *
 from random import randint
 
-def createTurtle(colour, track, name):
-    turtle=Turtle()
-    turtle.color(colour)
-    turtle.shape('turtle')
+names   = ['Ada','Ann',       'Kay',  'Aisha',  'Bob',  'Kev',   'Elliot', 'May',   'Andrew']
+colours = ['red','turquoise', 'pink', 'purple', 'blue', 'green', 'orange', 'black', 'yellow']
 
-    turtle.penup()
-    turtle.goto(-160, 100-30*track)
-    turtle.write(name)
-    turtle.pendown()
-    return turtle
-    
 
+# Show grid
 speed(10)
 penup()
 goto(-140, 140)
@@ -22,35 +14,61 @@ for step in range(15):
     right(90)
     forward(10)
     pendown()
-    forward (250)
+    forward(len(names) * 30 + 15)
     penup()
-    backward(260)
+    backward(len(names) * 30 + 25)
     left(90)
     forward(20)
 
-names=['ada','ann' ,'kay' ,'aisha' ,'bob','kev' , 'elliot' , 'may']
-colours=['red','turquoise', 'pink', 'purple', 'blue', 'green', 'orange', 'black']
+# Function to create a Turtle
+def createTurtle(colour, track, name):
+    turtle=Turtle()
+    turtle.color(colour)
+    turtle.shape('turtle')
+    turtle.penup()
+    turtle.goto(-220, 100-30*track)
+    turtle.write(name)
+    turtle.goto(-160, 100-30*track)
+    turtle.pendown()
+    return turtle
+
+# Create the collection of Turtles
 turtles=[]
 for i in range (len(names)):
     turtle=createTurtle(colours[i], i, names[i])
     turtles.append(turtle)
 
-#turtles=[ada,ann ,kay ,aisha ,bob,kev , elliot , may]
-w=0
-winner = 0
-leader = createTurtle('red', 9, 'leader')
+#Create and set up leader turtle
+leaderXpos = -1000
+winningTurtle = -1
+leader = createTurtle('red', len(turtles) + 1, 'leader')
+leader.hideturtle()
+leader.width(5)
 
+#Run the race
 for turn in range(100):
     i = 0
+    change = False
     for turtle in turtles :
+        turtle.forward(randint(1, 5))
+        [x, y] = turtle.position()
+        if x > leaderXpos:
+            leaderXpos = x
+            if (winningTurtle != i):
+                change = True
+                winningTurtle = i
         i = i + 1
-        turtle.forward(randint(1,5))
-        [x,y] = turtle.position()
-        if x>w:
-            w = x
-            winner=i
-    leader.write(names[winner])
-    
+    leader.goto(leaderXpos, 100 - 30 * (len(turtles) + 1))
+    if (change):
+        leader.color(colours[winningTurtle])
+        #leader.write(names[winningTurtle])
 
-
-print("the winner is", names[winner])
+#Report the winner
+leader.penup()
+leader.forward(30)
+leader.write("Winner = " + names[winningTurtle])
+leader.right(90)
+leader.forward(20)
+leader.left(90)
+leader.forward(30)
+leader.showturtle()
