23f36cd86c382a4a37ba22e08574f8883c705ada
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2019-02-09 12:34:12 +0000

    2019-02-09 Jake.

---
+++ Coders/jake/my_turtle.py
@@ -0,0 +1,26 @@
+from turtle import *
+from random import randint
+speed(10)
+penup()
+goto(-140, 140)
+
+for step in range(15):
+  write(step, align='center')
+  right(90)
+  forward(10)
+  pendown()
+  forward(150)
+  penup()
+  backward(160)
+  left(90)
+  forward(20)
+ada = Turtle()
+ada.color('red')
+ada.shape('turtle')
+
+ada.penup()
+ada.goto(-160, 100)
+ada.pendown()
+
+for turn in range(100)
+ ada.forward(randint(1,5))
