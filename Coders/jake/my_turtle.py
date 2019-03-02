efc8828c43caa87adb1696b854f06ca2cb2fba86
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2019-03-02 12:35:58 +0000

    2/3/18

--- Coders/jake/my_turtle.py
+++ Coders/jake/my_turtle.py
@@ -22,5 +22,24 @@ ada.penup()
 ada.goto(-160, 100)
 ada.pendown()
 
-for turn in range(100)
- ada.forward(randint(1,5))
+bob= Turtle()
+bob.color('blue')
+bob.shape('turtle')
+
+bob.penup()
+bob.goto(-160, 80)
+bob.pendown()
+
+ella = Turtle()
+ella.color('green')
+ella.shape('turtle')
+
+ella.penup()
+ella.goto(-160, 60)
+ella.pendown()
+
+for turn in range(100):
+  ada.forward(randint(1,5))
+  bob.forward(randint(1,5)) 
+  ella.forward(randint(1,5))
+
