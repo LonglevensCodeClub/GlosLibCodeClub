ef3a055f2860fdb740bb309fa81e23624a050ae6
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2019-04-27 11:31:46 +0000

    27/04/19

---
+++ Coders/jake/space invarders/eveil invaders.py
@@ -0,0 +1,44 @@
+import pgzrun
+
+player = Actor("player", (400,550))
+
+def draw():
+    screen.blit('background',(0, 0))
+    player.draw()
+    
+def update():
+    checkKeys()
+    
+def checkKeys():
+    global player
+    if keyboard.left:
+        if player.x > 40: player.x -= 5
+    if keyboard.right:
+        if player.x < 760: player.x += 5
+        
+        
+def updateAliens():
+    global moveSequence, movedelay
+    movex = movey = 0
+    if movesequence < 10 or movesequence > 30: movex = -15
+    if movesequence == 10 or movesequence == 30:
+        movey = 50
+    if movesequence >10 and movesequence < 30: movex =15
+    for a in range(len(aliens)): 
+        animate(aliens[a],
+                pos=(aliens[a].x + movex,
+                     aliens[a].y + movey),
+                duration=0.5, tween='linear')
+        if randint(0, 1) == 0:
+            aliens[a].image = "alien1"
+    
+        else:
+            aliens[a].image = "alien1b"
+    moveseqence +=1
+    if moveseqence == 40: moveseqence = 0
+        
+        
+
+pgzrun.go()            
+
+
