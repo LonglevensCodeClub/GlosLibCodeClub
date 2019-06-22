97a67a27722043a8c93135cc04966388ae8d37e2
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2019-06-22 12:32:24 +0100

    22/06

---
+++ Coders/Aisha/ColourMeDictionary.py
@@ -0,0 +1,24 @@
+
+
+from turtle import *
+
+screen = Screen()
+screen.setup(400, 400)
+
+colours = {
+    'verylime': '#A7E30E',
+    'reallyrasberry': '#FA057F'
+}
+
+screen.bgcolor(colours['verylime'])
+
+
+
+
+color('#FA057F')
+style = ('Arial', 40, 'bold')
+write('HELLO',font=style, align='center')
+hideturtle()
+
+
+
