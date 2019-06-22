b22c2b11f8ee3654471fdf6331baf39e9dfe144e
Loglevens Code Club - Pi 3 <longlevens.library.code.club@gmail.com>
2019-06-22 12:36:51 +0100

    2019 06 22 James W

---
+++ Coders/James W/Turtle Race.py
@@ -0,0 +1,32 @@
+from turtle import *
+penup()
+goto(-140,140)
+
+for step in range(6):
+    write(step)
+    right(90)
+    forward(10)
+    pendown()
+    forward(150)
+    penup()
+    backward(160)
+    left(90)
+    forward(20)
+        
+    
+#write(0)
+#forward(100)
+#write(5)
+#
+#write(0)
+#forward(20)
+#write(1)
+#forward(20)
+#write(2)
+#forward(20)
+#write(3)
+#forward(20)
+#write(4)
+#forward(20)
+#write(5)
+#forward(20)
