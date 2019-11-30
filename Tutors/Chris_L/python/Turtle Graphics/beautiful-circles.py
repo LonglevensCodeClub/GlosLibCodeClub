54cff18bf399be30fe2a6635d4572828751f2ffa
thechrislewis <chrislewis64@gmail.com>
2019-11-30 15:04:25 +0000

    updated to work with python2 and python3

--- Tutors/Chris_L/python/Turtle Graphics/beautiful-circles.py
+++ Tutors/Chris_L/python/Turtle Graphics/beautiful-circles.py
@@ -1,17 +1,17 @@
-from turtle import Turtle
+import turtle
 
-t=Turtle()
+t=turtle.Turtle()
+window = turtle.Screen()
 
-t.screen.bgcolor("black")
-t.screen.title("beautiful circles")
+window.bgcolor("black")
+window.title("beautiful circles")
 
 colors=["red","yellow","purple"]
-t.screen.tracer(0,0)
+window.tracer(0,0)
 
 for x in range(100):
   t.circle(x)
   t.color(colors[x%3])
   t.left(60)
 
-t.screen.exitonclick()
-t.screen.mainloop()
+window.mainloop()
--- Tutors/Chris_L/python/Turtle Graphics/beautiful-spiral.py
+++ Tutors/Chris_L/python/Turtle Graphics/beautiful-spiral.py
@@ -1,16 +1,20 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 t=Turtle()
-t.screen.bgcolor("black")
+window = Screen()
+
+window.bgcolor("black")
+window.title("Spirals")
 
 colors=["blue","purple","red","yellow","orange","brown"]
 
-t.screen.tracer(0,0)
+window.tracer(0,0)
 
 for x in range(300):
   t.color(colors[x%6])
   t.fd(x)
   t.left(59)
 
-t.screen.exitonclick()
-t.screen.mainloop()
+#keep window open until mouse clicked
+window.exitonclick()
+
