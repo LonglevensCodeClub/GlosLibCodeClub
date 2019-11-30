5356745d32dd1a1c80e220d5bb8b31dcfafa0d1f
thechrislewis <chrislewis64@gmail.com>
2019-11-30 15:28:03 +0000

    updated random draw

--- Tutors/Chris_L/python/Turtle Graphics/random-draw.py
+++ Tutors/Chris_L/python/Turtle Graphics/random-draw.py
@@ -1,23 +1,25 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 import random
  
 t=Turtle()
+window = Screen()
+window.title("Random walk")
+
 t.ht()
-t.screen.bgcolor("black")
-t.tracer(0)
+window.bgcolor("black")
+window.tracer(0,0)
 
 def random_drawing(turns,distance):
- 	for x in range(turns):
-	    right=t.right(random.randint(0,360))
- 	    left=t.left(random.randint(0,360))
- 	    t.color(random.choice(["blue","red","green"]))
- 	    random.choice([right,left])
- 	    t.fd(distance)
-	    t.screen.update()
+        for x in range(turns):
+                right=t.right(random.randint(0,360))
+                left=t.left(random.randint(0,360))
 
+                t.color(random.choice(["blue","red","green"]))
+                random.choice([right,left])
+                t.fd(distance)
+                window.update()
+       
 
-t.speed(0)
+random_drawing(1000,10)
 
-random_drawing(2000,5)
-
-t.screen.exitonclick()
+window.exitonclick()
