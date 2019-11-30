b5e4c7f807a83e75334fd2009c20622630e0b3f8
thechrislewis <chrislewis64@gmail.com>
2019-11-30 15:35:44 +0000

    improving consistency of code

---
+++ Tutors/Chris_L/python/Turtle Graphics/simple-rectangle.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+t=Turtle()
+t.screen.bgcolor("black")
+t.color("red")
+t.hideturtle()
+t.fd(100)
+t.left(90)
+t.fd(100)
+t.left(90)
+t.fd(100)
+t.left(90)
+t.fd(100)
+
+
+t.screen.exitonclick()
+
