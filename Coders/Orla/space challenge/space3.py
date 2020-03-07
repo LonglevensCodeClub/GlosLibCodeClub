723cd3f0512c7bb1dd2b5e1e7e6340c90ef8c753
Longlevens Library Raspberry Pi 1 <longlevens.library.code.club@gmail.com>
2020-03-07 12:32:52 +0000

    Orla & Isobel

--- Coders/Orla/space challenge/space3.py
+++ Coders/Orla/space challenge/space3.py
@@ -4,30 +4,22 @@ from time import sleep
 from sense_hat import SenseHat
 sense = SenseHat()
 sense.set_rotation(90)
+temp = round( sense.temperature, 1 )
 b=0,50,255
 bk=0,0,0
-r=50,0,0
+r=255,0,0
 sense.show_message("Hi from Natasha & Orla!", text_colour=b, back_colour=bk, scroll_speed=0.05)
 w=255,255,255
 p=255,102,155
 bl=102,153,255
 pr=102,0,204
 p2=255,26,140
-
-pic1 = [
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,
- r,b,b,b,b,b,b,b,]
+y=255,255,0
 
 picture = [w,w,pr,p,p,pr,w,w,
   w,p,w,p,p,w,p,w, 
   pr,w,w,p,p,w,w,pr,
-  pr,w,b,w,w,b,w,pr, 
+  pr,w,b,w,w,b,w,pr,
   pr,w,b,w,w,b,w,pr,
   pr,w,w,w,w,w,w,pr,
   pr,w,r,w,w,r,w,pr, 
@@ -36,18 +28,11 @@ sense.set_pixels(picture)
 sleep(2)
 temp = round( sense.temperature, 1 )
 sense.show_message( "It's " + str(temp) + " C" )
-b = min(255-(temp),255)
-g = 0
-r = min(0+(temp)+100,0)
-m = int(r),int(g),int(b)
 
-
-
-m = 255,0,0
-print(m)
-grid=[0]*64
-for i in range (64):
-    grid[i]=m
+if temp > 30:
+  grid = [(255,0,0)]*64
+elif temp<20:
+  grid=[b]*64
+else:
+  grid=[y]*64
 sense.set_pixels(grid)
-
-sense.set_pixels(pic1)
